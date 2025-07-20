f4c729c1d39f0a283baf342009410367a629a743
ueni.tech <ueni.tech@gmail.com>
2025-07-20 15:02:00 +0900

    Update dependencies and styles: added Radix UI components, class variance authority, and animation support; modified global styles for improved theming and layout; removed default page component.

---
+++ src/components/layouts/PublicHeader.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import SearchBox from "../post/SearchBox";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
+
+export default function PublicHeader() {
+  return (
+    <div>
+      <header className="border-b bg-blue-200">
+        <div className="container mx-auto px-4 py-4 flex items-center justify-between">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink asChild>
+                  <Link href="/" className="font-bold text-xl">blog</Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex items-center gap-4">
+            <SearchBox />
+            <Button variant="outline" asChild>
+              <Link href="/login">
+                ログイン
+              </Link>
+            </Button>
+            <Button variant="outline" asChild>
+              <Link href="/register">
+                登録
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </header>
+    </div>
+  )
+}
